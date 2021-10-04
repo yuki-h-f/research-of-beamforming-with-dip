42dd81063ce7c9d986a56718f823a3d406e9f56d
yuki-h-f <yuki.football@keio.jp>
2021-10-04 16:07:01 +0900

    add test images.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 
-beams = np.load("Echigo_dataset.npy")
-print(beams.shape)
-print(beams[0][0])
+beams1 = np.load("x_8_8.npy")
+beams2 = np.load("y_16_16.npy")
+print(beams1.shape)
+print(beams2.shape)
