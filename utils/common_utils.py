cd47d7ce2ef8c994cce3204dc4a998834150dfb3
yuki-h-f <yuki.football@keio.jp>
2021-10-19 11:46:55 +0900

    temp

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -78,7 +78,7 @@ def plot_image_grid(images_np, nrow =8, factor=1, interpolation='lanczos'):
     plt.figure(figsize=(len(images_np) + factor, 12 + factor))
     
     if images_np[0].shape[0] == 1:
-        plt.imshow(grid[0], cmap='gray', interpolation=interpolation)
+        plt.imshow(grid[0], cmap='Blues', interpolation=interpolation)
     else:
         plt.imshow(grid.transpose(1, 2, 0), interpolation=interpolation)
     
